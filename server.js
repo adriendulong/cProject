e0436ea76e3b968076f780fed1ccc82eecd80060
Adrien Dulong <adrien.dulong@gmail.com>
2016-11-30 17:37:45 +0100

    Modify the way to start processes in order to edploy on heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import {buildSchema} from 'graphql';
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.set('port', (process.env.API_PORT || 3001));
+
+app.use('/graphql',(req, res) => {
+  res.send({data: false});
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
